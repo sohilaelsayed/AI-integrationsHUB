c951f00c1778676b7826147fe6dde59fe26b629c
youssef-salama <youssifsalama47@gmail.com>
2026-05-09 23:41:48 +0300

    feat: Add CORS for specific origins

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.match_route import router
 
 app = FastAPI()
+
+# Allow specific origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://waffer.runasp.net",
+        "http://localhost:60614",
+        "http://localhost:5144",
+        "https://localhost:7007"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
